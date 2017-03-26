f86a427abde83cbd487c36ab1788708f717aeb16
RaymondLi0 <raymond_li@hotmail.fr>
2017-03-26 14:18:19 -0400

    rename some variables and change weight initialization

--- context_encoder.py
+++ context_encoder.py
@@ -7,6 +7,7 @@ import glob
 import PIL.Image as Image
 import time
 import params
+import functools
 
 
 def variable_summaries(var):
@@ -23,12 +24,13 @@ def variable_summaries(var):
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    n_input = functools.reduce(lambda x,y:y*x, shape[:-1])
+    initial = tf.truncated_normal(shape, stddev=np.sqrt(2/n_input))
     return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
 
@@ -58,7 +60,7 @@ class ContextEncoder(object):
         self.batch_size = batch_size
         self.nb_epochs = nb_epochs
         self._train_batch_index = batch_index
-        self.valid_batch_index = batch_index
+        self._valid_batch_index = batch_index
         self.mscoco = mscoco
         self.train_path = train_path
         self.valid_path = valid_path
@@ -119,29 +121,31 @@ class ContextEncoder(object):
                 variable_summaries(self._W_conv3)
             with tf.name_scope('biases'):
                 self._b_conv1 = bias_variable([64])
-                self.b_conv2 = bias_variable([64])
-                self.b_conv3 = bias_variable([128])
+                self._b_conv2 = bias_variable([64])
+                self._b_conv3 = bias_variable([128])
                 variable_summaries(self._b_conv1)
-                variable_summaries(self.b_conv2)
-                variable_summaries(self.b_conv3)
+                variable_summaries(self._b_conv2)
+                variable_summaries(self._b_conv3)
             self.h_conv1 = tf.nn.relu(conv2d(self.x_masked, self._W_conv1, stride=1) + self._b_conv1)
             self.h_pool1 = max_pool_2x2(self.h_conv1)
 
-            self.h_conv2 = tf.nn.relu(conv2d(self.h_pool1, self._W_conv2, stride=1) + self.b_conv2)
+            self.h_conv2 = tf.nn.relu(conv2d(self.h_pool1, self._W_conv2, stride=1) + self._b_conv2)
             self.h_pool2 = max_pool_2x2(self.h_conv2)
 
-            self.h_conv3 = tf.nn.relu(conv2d(self.h_pool2, self._W_conv3, stride=1) + self.b_conv3)
+            self.h_conv3 = tf.nn.relu(conv2d(self.h_pool2, self._W_conv3, stride=1) + self._b_conv3)
             self.h_pool3 = max_pool_2x2(self.h_conv3)
 
     def _channel_wise(self):
         with tf.name_scope('channel_wise'):
             with tf.name_scope('weights'):
-                self.W_fc1 = weight_variable([128, 8 * 8, 8 * 8])
+                self._W_fc1 = weight_variable([128, 8 * 8, 8 * 8])
+                variable_summaries(self._W_fc1)
             with tf.name_scope('biases'):
-                self.b_fc1 = bias_variable([128])
+                self._b_fc1 = bias_variable([128])
+                variable_summaries(self._b_fc1)
             self.h_pool3_flat_img = tf.reshape(self.h_pool3, [128, self.batch_size, 8 * 8])
             self.h_fc1 = tf.nn.relu(
-                tf.reshape(tf.matmul(self.h_pool3_flat_img, self.W_fc1), [self.batch_size, 64, 128]) + self.b_fc1)
+                tf.reshape(tf.matmul(self.h_pool3_flat_img, self._W_fc1), [self.batch_size, 64, 128]) + self._b_fc1)
 
             self.h_fc1_img = tf.reshape(self.h_fc1, [-1, 8, 8, 128])
 
@@ -149,28 +153,26 @@ class ContextEncoder(object):
         # print(self.h_fc1_img.get_shape().as_list())
         with tf.name_scope('decode'):
             with tf.name_scope('weights'):
-                self.W_uconv1 = weight_variable([5, 5, 64, 128])
-                self.W_uconv2 = weight_variable([5, 5, 64, 64])
+                self._W_uconv1 = weight_variable([5, 5, 64, 128])
+                self._W_uconv2 = weight_variable([5, 5, 64, 64])
             with tf.name_scope('biases'):
-                self.b_uconv1 = bias_variable([64])
-                self.b_uconv2 = bias_variable([64])
-            self.tmp = uconv2d(self.h_fc1_img, self.W_uconv1, output_shape=[self.batch_size, 16, 16, 64], stride=2)
-            # tf.Assert
+                self._b_uconv1 = bias_variable([64])
+                self._b_uconv2 = bias_variable([64])
             self.h_uconv1 = tf.nn.relu(
-                uconv2d(self.h_fc1_img, self.W_uconv1, output_shape=[self.batch_size, 16, 16, 64],
-                        stride=2) + self.b_uconv1)
+                uconv2d(self.h_fc1_img, self._W_uconv1, output_shape=[self.batch_size, 16, 16, 64],
+                        stride=2) + self._b_uconv1)
 
             self.h_uconv2 = tf.nn.relu(
-                uconv2d(self.h_uconv1, self.W_uconv2, output_shape=[self.batch_size, 32, 32, 64],
-                        stride=2) + self.b_uconv2)
+                uconv2d(self.h_uconv1, self._W_uconv2, output_shape=[self.batch_size, 32, 32, 64],
+                        stride=2) + self._b_uconv2)
 
     def _generate_image(self):
         with tf.name_scope('generated_image'):
-            self.W_uconv3 = weight_variable([5, 5, 3, 64])
-            self.b_uconv3 = bias_variable([3])
+            self._W_uconv3 = weight_variable([5, 5, 3, 64])
+            self._b_uconv3 = bias_variable([3])
             self.y = tf.nn.relu(
-                uconv2d(self.h_uconv2, self.W_uconv3, output_shape=[self.batch_size, 32, 32, 3],
-                        stride=1) + self.b_uconv3)
+                uconv2d(self.h_uconv2, self._W_uconv3, output_shape=[self.batch_size, 32, 32, 3],
+                        stride=1) + self._b_uconv3)
             self.y_padded = tf.pad(self.y, [[0, 0], [16, 16], [16, 16], [0, 0]])
             tf.summary.image("original_image", self.x, max_outputs=12)
             tf.summary.image("generated_image", self.y_padded + self.x_masked, max_outputs=12)
@@ -181,7 +183,6 @@ class ContextEncoder(object):
             tf.summary.scalar('reconstruction_loss', self._reconstruction_loss)
 
     def _optimize(self):
-        train_vars = [v for v in tf.trainable_variables() if v.name.startswith("generated_image")]
         with tf.variable_scope("optimizer"):
             optimizer = tf.train.AdamOptimizer()
             grads = optimizer.compute_gradients(self._reconstruction_loss)
@@ -216,7 +217,7 @@ class ContextEncoder(object):
     def _load_valid_batch(self):
         '''
         get next valid batch
-        TODO
+        TODO : merge load_batch functions and remove black images
         '''
         start_time = time.time()
         batch = np.zeros((self.batch_size, 64, 64, 3))
@@ -231,11 +232,27 @@ class ContextEncoder(object):
             else:
                 self.nb_bw_img += 1
 
-        self._valid_batch_index = (self._valid_batch_index + 1) % len(self.train_imgs)
+        N_valid_batch = len(self.valid_imgs) // self.batch_size
+        self._valid_batch_index = (self._valid_batch_index + 1) % N_valid_batch
 
         # print("batch loaded in : ", time.time() - start_time)
 
         return batch
+
+    def _load_valid_data(self):
+        '''
+        get valid data
+        :return:
+        '''
+        n_validation = len(self.valid_imgs) // 5
+        val_imgs = self.valid_imgs[:n_validation]
+
+        for i, img_path in enumerate(val_imgs):
+            img = Image.open(img_path)
+            if np.array(img).shape == (64, 64, 3):
+                val_imgs[i] = np.array(img)
+
+        return val_imgs
 
     def _restore(self):
         """
@@ -253,16 +270,20 @@ class ContextEncoder(object):
 
         self._sess.run(tf.global_variables_initializer())
 
-        summary_writer = tf.summary.FileWriter(self.logs_path,
-                                               graph=self._sess.graph,
-                                               flush_secs=20)
+        train_writer = tf.summary.FileWriter(os.path.join(self.logs_path, "train"),
+                                             graph=self._sess.graph,
+                                             flush_secs=20)
+        val_writer = tf.summary.FileWriter(os.path.join(self.logs_path, "val"),
+                                           graph=self._sess.graph,
+                                           flush_secs=20)
+
         if last_saved_model is not None:
             saver.restore(self._sess, last_saved_model)
             print("[*] Restoring model  {}".format(last_saved_model))
         else:
             tf.train.global_step(self._sess, self.global_step)
             print("[*] New model created")
-        return saver, summary_writer
+        return saver, train_writer, val_writer
 
     def _save(self, saver, summary_writer, is_iter=True, extras=None):
         """
@@ -280,7 +301,7 @@ class ContextEncoder(object):
         """
 
         current_iter = self._sess.run(self.global_step)
-        # Epoch saving (model)
+        # Epoch saving (logs + model)
         if not is_iter:
             # Save validation_accuracy
             summary_writer.add_summary(extras, global_step=current_iter)
@@ -288,7 +309,7 @@ class ContextEncoder(object):
             # Save graph
             saver.save(self._sess, global_step=current_iter, save_path=self.save_path)
 
-        # Iter saving (write variable + loss)
+        # Iter saving (logs)
         else:
             summary_writer.add_summary(extras, global_step=current_iter)
 
@@ -298,10 +319,11 @@ class ContextEncoder(object):
         :return:
         """
         # Retrieve a model or create a new
-        saver, summary_writer = self._restore()
+        saver, train_writer, val_writer = self._restore()
 
         epoch = 0
         n_train_batches = len(self.train_imgs) // self.batch_size
+        n_val_batches = len(self.valid_imgs) // self.batch_size // 2
 
         # Retrieve current global step
         last_step = self._sess.run(self.global_step)
@@ -329,21 +351,34 @@ class ContextEncoder(object):
 
                 if global_step % 200 == 0:
                     print("nb of black and white images so far : {}".format(self.nb_bw_img))
-                    self._save(saver, summary_writer, is_iter=False, extras=summary_str)
+
+                    self._save(saver, train_writer, is_iter=True, extras=summary_str)
                     # self._save(saver, summary_writer, is_iter=False, extras=None)
 
-            # self._save(saver, summary_writer, is_iter=False, extras=summary_str)
+            val_loss = 0
+            for i in tqdm(range(n_val_batches)):
+                batch = self._load_valid_batch()
+                loss = self._sess.run(self._reconstruction_loss, feed_dict={self.x: batch, self.mask: self.np_mask})
+                val_loss += loss
+
+            val_loss /= n_val_batches * self.batch_size
+            # self._save(saver, val_writer, is_iter=False, extras=summary_str)
+            val_writer.add_summary(
+                tf.Summary(value=[tf.Summary.Value(tag="val_loss", simple_value=val_loss), ])
+            )
             cprint("Epoch {}".format(epoch), color="yellow")
 
             epoch += 1
 
         cprint("Training done.", "green", attrs=["bold"])
 
-        summary_writer.flush()
-        summary_writer.close()
+        train_writer.flush()
+        val_writer.flush()
+        train_writer.close()
+        val_writer.close()
 
 
 if __name__ == '__main__':
-    ce = ContextEncoder(batch_size=32, nb_epochs=30)
+    ce = ContextEncoder(batch_size=32, nb_epochs=50)
     ce.build_model()
     ce.train()
